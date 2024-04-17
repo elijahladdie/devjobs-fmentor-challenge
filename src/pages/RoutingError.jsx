89cfadfd93899e6b25f88745d4eea6b2e648d74b
elijah <elijahladdie2004@gmail.com>
2024-04-18 00:35:13 +0200

    adding login and signup form

--- src/pages/RoutingError.jsx
+++ src/pages/RoutingError.jsx
@@ -1,14 +1,12 @@
-import { useRouteError } from "react-router-dom";
 
 const RoutingError = () => {
-  const error = useRouteError();
-  //   console.log(error);
+ 
 
   return (
     <div className="error-page">
       <h3>Sorry... there seems to be an error</h3>
       <p className="error-text">
-        {error.status} - {error.statusText}
+        404 - Not Found
       </p>
     </div>
   );
